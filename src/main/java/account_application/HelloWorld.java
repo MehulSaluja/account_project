8cf746b9ddd116f798c33721db4bc3d1e72daaf0
Mehul Saluja <saluja.mehul@gmail.com>
2018-11-13 10:25:32 +0000

    first commit

---
+++ src/main/java/account_application/HelloWorld.java
@@ -0,0 +1,10 @@
+package account_application;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world to the standard out");
+	}
+
+}
